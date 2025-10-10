32e6fdd4715f11c7fa2b1016aff7aff9aee2ac9f
Tiderise <sumarakcrni@gmail.com>
2025-10-11 00:10:17 +0200

    Quartz sync: Oct 11, 2025, 12:10 AM

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "sumarakcrni-tech.github.io/rise-of-the-tide",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
